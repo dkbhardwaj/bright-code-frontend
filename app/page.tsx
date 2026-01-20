4d7576c12c9dcc1450abe2bec9b6bab2952c3819
Anoop <anoop@cleardigital.com>
2026-01-20 13:35:35 +0530

    add nextjs folder structure

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+       
+        <div className="flex flex-col items-center gap-6 text-center w-full">
+          <h1 className="max-w-xs text-center text-[50px] font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Bright Code
+          </h1>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
