8ded657623eaabbb1fe67718c32b0d141429e453
Ganesh <ganesh@cleardigital.com>
2026-01-30 20:49:09 +0530

    Added Navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "../styles/buttons.css";
+import Navigation from "../components/navigation";
 import Footer from "../components/footer"
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation />
         {children}
         <Footer />
       </body>
---
+++ components/navigation-data.ts
@@ -0,0 +1,166 @@
+
+export const NAV_DATA = {
+    menus: [
+      {
+        label: "Services",
+        theme: "both",
+        mega: {
+          columns: [
+            [
+              {
+                imageLight: "/header/secure-file.svg",
+                imageDark: "/header/secure-file-dark.svg",
+                alt: "Secure file icon",
+                title: "White Label Website",
+                desc: "Test Your Security Headers",
+                icon: "card",
+                link: "/services/white-label-website",
+              },
+              {
+                imageLight: "/header/link-icon.svg",
+                imageDark: "/header/link-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Legacy Refraction",
+                desc: "Track URL Redirects",
+                icon: "link",
+                link: "/services/legacy-refraction",
+              },
+              {
+                imageLight: "/header/image-icon.svg",
+                imageDark: "/header/image-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Care & Optimization",
+                desc: "Free Image Checkers",
+                icon: "image",
+                link: "/services/care-optimization",
+              },
+            ],
+            [
+              {
+                imageLight: "/header/secure-file.svg",
+                imageDark: "/header/secure-file-dark.svg",
+                alt: "Secure file icon",
+                title: "Dev Squad Support",
+                desc: "Test Your Security Headers",
+                icon: "card",
+                link: "/services/dev-squad-support",
+              },
+              {
+                imageLight: "/header/image-icon.svg",
+                imageDark: "/header/image-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Image Checker",
+                desc: "Free Image Checkers",
+                icon: "image",
+                link: "/tools/image-checker",
+              },
+              {
+                imageLight: "/header/link-icon.svg",
+                imageDark: "/header/link-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Link Tracker",
+                desc: "Track URL Redirects",
+                icon: "link",
+                link: "/tools/link-tracker",
+              },
+            ],
+          ],
+        },
+      },
+  
+      {
+        label: "Case Studies",
+        theme: "dark",
+        mega: {
+          columns: [
+            [
+              {imageLight: "/header/secure-file.svg",
+                imageDark: "/header/secure-file-dark.svg",
+                alt: "Secure file icon",
+                title: "Team ADDX Agency", 
+                desc: "Test Your Security Headers", 
+                link: "/case-studies/addx" 
+            },
+              { imageLight: "/header/link-icon.svg",
+                imageDark: "/header/link-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Flyingduck Agency", 
+                desc: "Track URL Redirects", 
+                link: "/case-studies/flyingduck" 
+            },
+            {   imageLight: "/header/image-icon.svg",
+                imageDark: "/header/image-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Planck Network", 
+                desc: "Free Image Checkers", 
+                link: "/case-studies/planck" 
+            },
+            ],
+            [
+              { 
+                imageLight: "/header/secure-file.svg",
+                imageDark: "/header/secure-file-dark.svg",
+                alt: "Secure file icon",
+                title: "Microsoft", 
+                desc: "Test Your Security Headers", 
+                link: "/case-studies/microsoft" },
+              { 
+                imageLight: "/header/image-icon.svg",
+                imageDark: "/header/image-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Amazon", 
+                desc: "Free Image Checkers", 
+                link: "/case-studies/amazon" },
+              { 
+                imageLight: "/header/link-icon.svg",
+                imageDark: "/header/link-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Google", 
+                desc: "Track URL Redirects", 
+                link: "/case-studies/google" 
+            },
+            ],
+          ],
+        },
+      },
+  
+      {
+        label: "Try Our Tools",
+        theme: "dark",
+        mega: {
+          columns: [
+            [
+              { 
+                imageLight: "/header/secure-file.svg",
+                imageDark: "/header/secure-file-dark.svg",
+                alt: "Secure file icon",
+                title: "Security Header", 
+                desc: "Test Your Security Headers", 
+                link: "/tools/security-header" 
+            },
+              { 
+                imageLight: "/header/link-icon.svg",
+                imageDark: "/header/link-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Link Tracker", 
+                desc: "Track URL Redirects", 
+                link: "/tools/link-tracker" 
+            },
+              { 
+                imageLight: "/header/image-icon.svg",
+                imageDark: "/header/image-icon-dark.svg",
+                alt: "Secure file icon",
+                title: "Image Checker", 
+                desc: "Free Image Checkers", 
+                link: "/tools/image-checker" 
+            },
+            ],
+          ],
+        },
+      },
+  
+      { label: "About us", link: "/about" },
+      { label: "We Do", link: "/what-we-do" },
+    ],
+  };
+  
