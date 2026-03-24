a93e712a1b23bc573db6823af5a51742c7720634
Ganesh <ganesh@cleardigital.com>
2026-03-24 21:57:09 +0530

    chore: Ignore TS error for eslint config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const wpUrl = process.env.NEXT_PUBLIC_WP_GRAPHQL_URL ? new URL(process.env.NEXT_
 
 
 const nextConfig: NextConfig = {
+  // @ts-ignore
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
